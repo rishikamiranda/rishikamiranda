e60c94f0fc7b1a6ce8fe50c4935f5679f77ff669
toxac <derocon@gmail.com>
2026-09-03 20:46:53 +0530

    updated client

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,8 +5,8 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
 export async function createClient() {
   const cookieStore = await cookies();
